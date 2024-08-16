f6b344997acfd4e01fab1161df7224c47e244772
yuki-tsurut <nfcgt.30@gmail.com>
2024-08-16 21:40:12 +0900

    エンドポイントとテストの追加

--- ktor-crud-web-api/src/main/kotlin/example/com/plugins/Routing.kt
+++ ktor-crud-web-api/src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,13 +1,23 @@
 package example.com.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/content", "mycontent")
+
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
         }
     }
 }
